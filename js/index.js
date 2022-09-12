eecd3a9760423e484090e0bd5873e58e32e90e9b
Zied Ben Amor <zied-ci@hotmail.fr>
2022-09-12 16:29:53 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-function myFunction(){
-var menuBtn = document.getElementById("menu-button");
-menuBtn.addEventListener("click", myFunction);
-}
+const { log } = console;
+
+  console.log("DOM fully loaded and parsed");
+  var menuBtn = document.getElementById("menu-button");
+  var closeBtn = document.getElementById("close-btn");
+var menu = document.getElementById("nav-items");
+  var links = document.getElementsByClassName(".inter-link");
+menuBtn.addEventListener("click", function () {
+  menu.classList.add("menu-shown");
+  closeBtn.style.display = "block";
+});
+  closeBtn.addEventListener("click", function () { 
+  closeBtn.style.display = "none";
+  menu.classList.remove("menu-shown");
+  }
+);
+menu.addEventListener("click", function () {
+  closeBtn.style.display = "none";
+  menu.classList.remove("menu-shown");
+});
+
+
+
